07ff7b9f06b68a83c5ab557c75b64fabafed9204
nitrix <me@nitrix.one>
2019-11-25 14:44:41 +0100

    Implement return statement node

---
+++ src/main/java/de/hsrm/compiler/Klang/nodes/statements/ReturnStatement.java
@@ -0,0 +1,19 @@
+package de.hsrm.compiler.Klang.nodes.statements;
+
+import de.hsrm.compiler.Klang.nodes.expressions.Expression;
+import de.hsrm.compiler.Klang.visitors.Visitor;
+
+public class ReturnStatement extends Statement {
+
+    public Expression expression;
+
+    public ReturnStatement(Expression expression) {
+        this.expression = expression;
+    }
+
+    @Override
+    public <R> R welcome(Visitor<R> v) {
+        return v.visit(this);
+    }
+
+}
