12c1f75602c15684d869415bba5ca25ad5e38dc3
nitrix <me@nitrix.one>
2020-02-03 22:20:25 +0100

    add fields to save the line and column in which the token corresponding to the node was found

--- src/main/java/de/hsrm/compiler/Klang/nodes/Node.java
+++ src/main/java/de/hsrm/compiler/Klang/nodes/Node.java
@@ -5,5 +5,8 @@ import de.hsrm.compiler.Klang.visitors.*;
 
 public abstract class Node {
     public Type type;
+    public int line;
+    public int col;
+
     public abstract <R> R welcome(Visitor<R> v);
 }
