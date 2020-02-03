fc33ab6b1289e7bb0c0a730ca323aa9225218423
nitrix <me@nitrix.one>
2020-02-03 23:42:07 +0100

    add method to get the raw value representation

--- src/main/java/de/hsrm/compiler/Klang/Value.java
+++ src/main/java/de/hsrm/compiler/Klang/Value.java
@@ -1,10 +1,16 @@
 package de.hsrm.compiler.Klang;
 
+import de.hsrm.compiler.Klang.types.Type;
+
 public class Value {
     private Object value;
 
     public Value(Object value) {
         this.value = value;
+    }
+
+    public Object asObject() {
+        return this.value;
     }
 
     public int asInteger() {
