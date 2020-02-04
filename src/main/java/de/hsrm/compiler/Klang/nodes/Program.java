6a7eb8fde2c13b0a5f7cc4a9afa761e0814dc0da
nitrix <me@nitrix.one>
2020-02-04 21:29:25 +0100

    make GetStructs collect the complete struct definitions

--- src/main/java/de/hsrm/compiler/Klang/nodes/Program.java
+++ src/main/java/de/hsrm/compiler/Klang/nodes/Program.java
@@ -1,15 +1,17 @@
 package de.hsrm.compiler.Klang.nodes;
 
+import java.util.Map;
+
 import de.hsrm.compiler.Klang.nodes.expressions.Expression;
 import de.hsrm.compiler.Klang.visitors.Visitor;
 
 public class Program extends Node {
   
   public FunctionDefinition[] funcs;
-  public StructDefinition[] structs;
+  public Map<String, StructDefinition> structs;
   public Expression expression;
 
-  public Program(FunctionDefinition[] funcs, StructDefinition[] structs, Expression expression) {
+  public Program(FunctionDefinition[] funcs, Map<String, StructDefinition> structs, Expression expression) {
     this.funcs = funcs;
     this.structs = structs;
     this.expression = expression;
